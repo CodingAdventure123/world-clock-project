37531c5a6dbef8ee0256c33b446179b3615f7f97
CodingAdventure123 <105638595+CodingAdventure123@users.noreply.github.com>
2023-09-17 11:14:23 +0200

    added current time
    current time/timezone as an option in the select

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -30,6 +30,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current-timezone") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment.tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
