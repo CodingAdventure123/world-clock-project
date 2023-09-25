b8a690e5fb76150795ee9783a554ffc74010c673
CodingAdventure123 <105638595+CodingAdventure123@users.noreply.github.com>
2023-09-25 15:53:34 +0200

    fixed formatting bug in JS

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -50,14 +50,16 @@ function updateCity(event) {
   let cityTime = moment.tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
-  <div>
-    <h2>${cityName}</h2>
-    <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
-  </div>
-  <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
     "A"
   )}</small></div>
   </div>
+
   <a class="linkCities" href="/">Back to all cities</a>
   `;
 }
